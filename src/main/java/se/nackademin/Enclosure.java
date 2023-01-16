d89220313055724f7a86bdee13a0a285d78e3b05
lundgren98 <a.k.o.lundgren@gmail.com>
2023-01-16 09:38:56 +0100

    Enclosure is now green

--- src/main/java/se/nackademin/Enclosure.java
+++ src/main/java/se/nackademin/Enclosure.java
@@ -2,14 +2,33 @@ package se.nackademin;
 
 class Enclosure {
 	private double size; // in sq ft
-	public boolean dirty;
+	private boolean dirty;
 	private double temperature;
+	private boolean open;
 
 	public double getSize() { return this.size; }
-	public boolean getDirty() { return this.dirty; }
-	public double getTemperature() { return this.temperature; }
+	public void setSize(double size) { this.size = size; }
 
-	public void clean() {}
-	public void close() {}
-	public void open() {}
+	public boolean getDirty() { return this.dirty; }
+	public void setDirty(boolean dirty) { this.dirty = dirty; }
+
+	public double getTemperature() { return this.temperature; }
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+
+	public boolean getOpen() { return this.open; }
+	public void setOpen(boolean open) { this.open = open; }
+
+	public void clean() {
+		this.setDirty(false);
+	}
+
+	public void close() {
+		this.setOpen(false);
+	}
+
+	public void open() {
+		this.setOpen(true);
+	}
 }
